cc77d252258b11216b9e77a0d76a524db2f50e61
hankn01 <risingstaruj@gmail.com>
2023-11-23 01:02:16 +0900

    이식 완료 후 부서장급 직원 검색 기능 추가

--- RECRUIT_DB/src/test.java
+++ RECRUIT_DB/src/test.java
@@ -69,44 +69,120 @@ public class test {
 
     public static void announcementManagement(Connection conn, Statement stmt){
         // System.out.println("announcementManagement works@");
-        String sql = "SELECT * FROM POST_INFO WHERE POST_ID IN (SELECT POST_ID FROM ANNOUNCEMENT_INFO WHERE MANAGER_ID = '"+ID+"') AND TYPE = 'A'";
-
-        // check if the user exists
-        try {
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery(sql);
-            if (!rs.next()) {
-                System.out.println("인증에 실패하였습니다. 프로그램을 종료합니다.");
-                System.exit(1);
+        boolean announcementMenuLoop = true;
+        int ann_menu = 0;
+        Scanner sc = new Scanner(System.in);
+        String sql = null;
+        while(announcementMenuLoop) {
+            stmt = null;
+            rs = null;
+            System.out.println("[메뉴 선택]");
+            System.out.println("1. 현재 ID의 전체 게시글 검색");
+            System.out.println("2. 게시글을 올린 직원 중 부서장급 직책의 직원을 조건에 따라 검색");
+            System.out.println("3. 프로그램 종료");
+            System.out.print("> ");
+            ann_menu = sc.nextInt();
+            if(1> ann_menu || ann_menu > 3){
+                System.out.println("Wrong selection. Please Try again!");
+                continue;
             }
-            else {
-                System.out.printf("%-12s|%-10s|%-10s|%-5s|\n", "POST ID", "Posted", "Updated", "Views");
-                System.out.println("------------+----------+----------+-----|");
-                while(rs.next())
-                {
-                    String postID = rs.getString(1);
-                    java.util.Date postedDate = rs.getDate(2);
-                    java.util.Date updateDate = rs.getDate(3);
-                    int views = rs.getInt(4);
+            if(ann_menu==1)
+            {
+                sql = "SELECT * FROM POST_INFO WHERE POST_ID IN (SELECT POST_ID FROM ANNOUNCEMENT_INFO WHERE MANAGER_ID = '"+ID+"') AND TYPE = 'A'";
+
+                // check if the user exists
+                try {
+                    stmt = conn.createStatement();
+                    rs = stmt.executeQuery(sql);
+                    if (!rs.next()) {
+                        System.out.println("인증에 실패하였습니다. 프로그램을 종료합니다.");
+                        System.exit(1);
+                    }
+                    else {
+                        System.out.printf("%-12s|%-10s|%-10s|%-5s|\n", "POST ID", "Posted", "Updated", "Views");
+                        System.out.println("------------+----------+----------+-----|");
+                        while(rs.next())
+                        {
+                            String postID = rs.getString(1);
+                            java.util.Date postedDate = rs.getDate(2);
+                            java.util.Date updateDate = rs.getDate(3);
+                            int views = rs.getInt(4);
 
 
-                    System.out.print(postID);
-                    System.out.print("|");
-                    System.out.print(postedDate);
-                    System.out.print("|");
-                    System.out.print(updateDate);
-                    System.out.print("|");
-                    System.out.print(views);
-                    System.out.println("|");
+                            System.out.print(postID);
+                            System.out.print("|");
+                            System.out.print(postedDate);
+                            System.out.print("|");
+                            System.out.print(updateDate);
+                            System.out.print("|");
+                            System.out.print(views);
+                            System.out.println("|");
 
+                        }
+                        System.out.println("------------+----------+----------+-----|");
+                    }
+                } catch (SQLException e) {
+                    System.out.println("Error: " + e.getMessage());
+                    System.exit(1);
                 }
-                System.out.println("------------+----------+----------+-----|");
             }
-        } catch (SQLException e) {
-            System.out.println("Error: " + e.getMessage());
-            System.exit(1);
-        }
+            else if(ann_menu==2) {
+                System.out.println("=====[공지를 관리할 수 있는 부서 관리자급 직원 검색]=====");
+                System.out.println("=검색 옵션 선택=");
+                System.out.println("1. 이름으로 검색");
+                System.out.println("2. 직원 부서로 검색");
+                System.out.println("3. 전체 검색");
+                System.out.print("> ");
+                int a2_option = sc.nextInt();
+                if(a2_option==1)
+                {
+                    System.out.print("직원 이름 입력: ");
+                    String a2_name = sc.next();
+                    sql = "SELECT DISTINCT E.NAME, D.CONTACT, D.DEPARTMENT_ID FROM EMPLOYEE_INFO E, DEPARTMENT_INFO D, ANNOUNCEMENT_INFO A WHERE E.ID = D.HEAD_ID AND A.MANAGER_ID = E.ID AND E.NAME='"+a2_name+"'";
+                }
+                else if(a2_option==2)
+                {
+                    System.out.print("직원 부서 입력: ");
+                    String a2_id = sc.next();
+                    sql = "SELECT DISTINCT E.NAME, D.CONTACT, D.DEPARTMENT_ID FROM EMPLOYEE_INFO E, DEPARTMENT_INFO D, ANNOUNCEMENT_INFO A WHERE E.ID = D.HEAD_ID AND A.MANAGER_ID = E.ID AND D.DEPARTMENT_ID='"+a2_id+"'";
+                }
+                else if(a2_option==3)
+                {
+                    sql = "SELECT DISTINCT E.NAME, D.CONTACT, D.DEPARTMENT_ID FROM EMPLOYEE_INFO E, DEPARTMENT_INFO D, ANNOUNCEMENT_INFO A WHERE E.ID = D.HEAD_ID AND A.MANAGER_ID = E.ID";
+                }
 
+                // check if the user exists
+                try {
+                    stmt = conn.createStatement();
+                    rs = stmt.executeQuery(sql);
+                        System.out.printf("%-4s|%-10s|%-4s|\n","이름", "전화번호(Phone)", "부서");
+                        System.out.println("=========================");
+
+                        while(rs.next())
+                        {
+                            String Name = rs.getString(1);
+                            String Phone = rs.getString(2);
+                            String Department = rs.getString(3);
+
+                            System.out.print(Name);
+                            System.out.print("|");
+                            System.out.print(Phone);
+                            System.out.print("|");
+                            System.out.print(Department);
+                            System.out.println("|");
+
+                        }
+                        System.out.println("=========================");
+                } catch (SQLException e) {
+                    System.out.println("Error: " + e.getMessage());
+                    System.exit(1);
+                }
+            }
+            else if(ann_menu==3)
+            {
+                announcementMenuLoop = false;
+            }
+        }
 
     }
 
